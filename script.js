45736d4d6118aefc849b154f674891fdca9a2f73
André-git <andre.potthast.ap@gmail.com>
2023-02-20 14:29:35 +0100

    linked script and added autodelete for non number in tel input

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // comment under every input when the input does not fit
+
+
+//prevent input of non numbers or use a pattern with a description.
+const telInput= document.getElementById("tel"); 
+telInput.addEventListener("input",removeNonNumeric)
+
+function removeNonNumeric() {
+    console.log(this.value)
+    this.value = this.value.replace(/[^0-9]/g, "");
+    //todo add an output error after 3*times non number?
+  }
